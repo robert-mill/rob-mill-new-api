1acc611a0d3f912da1f460c0d7b2aa17d8f012bf
Robert Mill <r.mill@ntlworld.com>
2022-07-06 14:27:01 +0100

    replaced mongodb with sqlite3

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var md5 = require("md5")
 var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-var HTTP_PORT = 8000
+const HTTP_PORT = process.env.PORT || config.get("port");
 app.listen(HTTP_PORT, () => {
     console.log("Server running on port %PORT%".replace("%PORT%",HTTP_PORT))
 });
