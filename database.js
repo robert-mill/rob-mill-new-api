b100d944995497355eda6c44559e6ebf4ee64b43
Robert Mill <r.mill@ntlworld.com>
2022-07-07 19:35:32 +0100

    updated api

--- database.js
+++ database.js
@@ -20,8 +20,14 @@ let db = new sqlite3.Database(DBSOURCE, (err) => {
         if (err) {
         }else{
             var insert = 'INSERT INTO cpp (instruction, description, example, result) VALUES (?,?,?,?)'
-            db.run(insert, ["In terminal type \‘g++\’","If error everything is ok else If not say yes to questions","g++"])
-            db.run(insert, ["Open visual studio code"," create new folder called C++, or your project name",""])
+            db.run(insert, ["In terminal type \‘g++\’",
+                           "If error everything is ok else If not say yes to questions",
+                           "g++",""])
+            db.run(insert, [
+                            "Open visual studio code",
+                            " create new folder called C++, or your project name",
+                            "",
+                            ""])
         }
     })  
     }
